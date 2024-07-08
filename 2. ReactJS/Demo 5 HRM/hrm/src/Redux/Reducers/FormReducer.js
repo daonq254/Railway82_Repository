0596de900330f80f00ba3989d93fda867f439b82
daonq254 <dao.nq254@gmail.com>
2024-07-08 21:56:21 +0700

    20240708_3

--- 2. ReactJS/Demo 5 HRM/hrm/src/Redux/Reducers/FormReducer.js
+++ 2. ReactJS/Demo 5 HRM/hrm/src/Redux/Reducers/FormReducer.js
@@ -3,6 +3,8 @@ import { CLOSE_INPUT_FORM, SHOW_INPUT_FORM } from "../Contants/ActionType";
 // Bước 3: Khai báo ra các State cần quản lý trong Store
 let initialstate = {
   showForm: false,
+  abc: "daonq",
+  xzy: 10,
 };
 
 // Bước  2: Khai báo Reducer
